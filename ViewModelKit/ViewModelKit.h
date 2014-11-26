85e23438d96fdeba7bd9e85005d38371617770f5
Raheel Ahmad <ahmadraheel@gmail.com>
2014-11-26 08:35:27 -0800

    Initial Commit

---
+++ ViewModelKit/ViewModelKit.h
@@ -0,0 +1,19 @@
+//
+//  ViewModelKit.h
+//  ViewModelKit
+//
+//  Created by Raheel Ahmad on 11/26/14.
+//  Copyright (c) 2014 Sakun Labs. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for ViewModelKit.
+FOUNDATION_EXPORT double ViewModelKitVersionNumber;
+
+//! Project version string for ViewModelKit.
+FOUNDATION_EXPORT const unsigned char ViewModelKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ViewModelKit/PublicHeader.h>
+
+
